38c72d1d39c72e30b0778c1747ad803580a26120
Akemi 3 <akemisnoww@gmail.com>
2024-09-29 18:05:38 -0300

    Add files via upload
    Alguns códigos básicos que eu re-aprendi

---
+++ Primeiros Passos.py
@@ -0,0 +1,17 @@
+# primeiros códigos
+print("Olá Mundo <3")
+
+#variáveis
+
+nome= 'Akemi'
+idade= '20'
+peso= '?'
+print(nome, idade, peso)
+
+#interatividade
+
+nome = input('qual é seu nome?')
+idade = input('qual a sua idade?')
+peso =  input('qual o seu peso?')
+print(nome, idade, peso)
+
